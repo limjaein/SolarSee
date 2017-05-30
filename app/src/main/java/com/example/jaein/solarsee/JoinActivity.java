86c0ecf2f031b9e15b7951017da8748bbd92eb3c
Unknown <nmilkjh2889@naver.com>
2017-05-30 22:38:47 +0900

    회원가입완료르르르

--- app/src/main/java/com/example/jaein/solarsee/JoinActivity.java
+++ app/src/main/java/com/example/jaein/solarsee/JoinActivity.java
@@ -1,21 +1,32 @@
 package com.example.jaein.solarsee;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import static com.example.jaein.solarsee.LoginActivity.font;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Member;
 
 public class JoinActivity extends AppCompatActivity {
 
     EditText joinID;
     EditText joinPw;
     EditText joinNickname;
-
-    Button idBtn, nickBtn, joinBtn;
+    DatabaseReference memberInfo;
+    DatabaseReference photoInfo;
+    FirebaseDatabase fDatabase;
+    Button completeJoinBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +41,12 @@ public class JoinActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         joinID = (EditText)findViewById(R.id.joinID);
+        joinPw = (EditText)findViewById(R.id.joinPW);
         joinNickname = (EditText)findViewById(R.id.joinNickname);
-        idBtn = (Button)findViewById(R.id.idcheckBtn);
-        nickBtn = (Button)findViewById(R.id.nickcheckBtn);
-        joinBtn = (Button)findViewById(R.id.joinBtn);
-        idBtn.setTypeface(font);
-        nickBtn.setTypeface(font);
-        joinBtn.setTypeface(font);
+
+        fDatabase = FirebaseDatabase.getInstance();
+        memberInfo = fDatabase.getReference("SolarSee/MEMBER_INFO");
+        photoInfo = fDatabase.getReference("SolarSee/PHOTO_INFO");
     }
 
     public void joinBtnClick(View view) {
@@ -47,27 +57,83 @@ public class JoinActivity extends AppCompatActivity {
             case R.id.nickcheckBtn:
                 nickCkeck();
                 break;
-            case R.id.joinBtn:
+            case R.id.completeJoin:
                 join();
                 break;
         }
     }
 
     public void idCheck(){
-        String idTxt = joinID.getText().toString();
 
         ////////디비 아이디들이랑 비교/////////
+        final String j_id = joinID.getText().toString();
+        //select *from customer where customer id='banana';
+        Query query = memberInfo.orderByChild("m_id").equalTo(j_id);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getValue()==null){
+                    Toast.makeText(JoinActivity.this, "사용 가능한 아이디입니다.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    for (DataSnapshot data : dataSnapshot.getChildren()){
+                        member item = data.getValue(member.class);
+                        if(j_id.equals(item.getM_id())){
+                            Toast.makeText(JoinActivity.this, "이미 사용중인 아이디입니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
     public void nickCkeck(){
-        String nickTxt = joinNickname.getText().toString();
-
+        final String j_name = joinNickname.getText().toString();
+        Query query = memberInfo.orderByChild("m_name").equalTo(j_name);
         ////////디비 닉네임들이랑 비교//////////
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getValue()==null){
+                    Toast.makeText(JoinActivity.this, "사용 가능한 닉네임입니다.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    for (DataSnapshot data : dataSnapshot.getChildren()){
+                       member item = data.getValue(member.class);
+                        if(j_name.equals(item.getM_name())){
+                            Toast.makeText(JoinActivity.this, "이미 사용중인 닉네임입니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
     public void join(){
         String id = joinID.getText().toString();
         String pw = joinPw.getText().toString();
         String nick = joinNickname.getText().toString();
 
+
+
         /////////////디비에 저장/////////////////
+        member member = new member(id, nick, pw);
+        memberInfo.child(id).setValue(member);
+        Toast.makeText(this, "디비저장 완료", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent();
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/jaein/solarsee/LoginActivity.java
+++ app/src/main/java/com/example/jaein/solarsee/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.example.jaein.solarsee;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,97 +8,55 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-
 public class LoginActivity extends AppCompatActivity {
-    DatabaseReference DBtable;
-    DatabaseReference t_member; // 전부다 저장해두기
-    DatabaseReference t_photo; // 입력받는거 만들어야함
-    private FirebaseAuth mAuth;
-    EditText et_id, et_pw;
-    String str_id, str_pw;
-    int JOIN_OK = -100;
-    static Typeface font;
-    Button btn1,btn2;
+
+    EditText loginId, loginPw;
+    Button joinBtn;
+
+    int JOIN_OK = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         init();
-        initDB();
+
+        joinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, JoinActivity.class);
+                startActivityForResult(intent, JOIN_OK);
+            }
+        });
     }
 
-    private void init() {
-        font = Typeface.createFromAsset(getAssets(), "font.ttf");
-        et_id = (EditText)findViewById(R.id.et_id);
-        et_pw = (EditText)findViewById(R.id.et_pw);
-        mAuth = FirebaseAuth.getInstance();
-        mAuth.signInWithEmailAndPassword("dlawodls96@naver.com", "ss9436022");
-        btn1 = (Button)findViewById(R.id.loginBtn);
-        btn2 = (Button)findViewById(R.id.joinBtn);
-        btn1.setTypeface(font);
-        btn2.setTypeface(font);
-    }
-    private void initDB() {
-        DBtable = FirebaseDatabase.getInstance().getReference("SolarSee");
-        t_member = DBtable.child("MEMBER_INFO");
-        t_photo = DBtable.child("PHOTO_INFO");
+    public void init(){
+        loginId = (EditText)findViewById(R.id.loginId);
+        loginPw = (EditText)findViewById(R.id.loginPw);
+        joinBtn = (Button)findViewById(R.id.joinBtn);
+
     }
 
-    public void joinBtnClick(View view) {
-        Intent intent = new Intent(this, JoinActivity.class);
-        startActivity(intent);
-    }
+//    public void joinBtnClick(View view) {
+//        Intent intent = new Intent(this, JoinActivity.class);
+//        startActivityForResult(intent, JOIN_OK);
+//        //startActivity(intent);
+//    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == JOIN_OK){
             if(resultCode == RESULT_OK){
+                Toast.makeText(this, "회원가입완료", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    public void loginBtnClick(View view){
+    public void loginBtnClick(View view) {
         //DB와 비교
-        str_id = et_id.getText().toString();
-        str_pw = et_pw.getText().toString();
 
-        Query query = t_member.orderByKey().equalTo(str_id);
 
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot data : dataSnapshot.getChildren()){
-                    member m = data.getValue(member.class);
-                    if(str_pw.equals(m.getM_pw())){ // 비밀번호 일치
-                        Toast.makeText(LoginActivity.this, "일치", Toast.LENGTH_SHORT).show();
-                        intentToMain();
-                    }
-                    else{
-                        // editText 초기화
-                        et_id.setText("");
-                        et_pw.setText("");
-                        Toast.makeText(LoginActivity.this, "일치하는 정보가 없습니다.", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-    }
-    public void intentToMain(){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        //mainActivity로 넘기기
     }
 }
