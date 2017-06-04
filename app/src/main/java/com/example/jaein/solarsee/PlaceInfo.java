c0d56bc3e8a670fd0ed5df624648928f422fc091
Unknown <nmilkjh2889@naver.com>
2017-06-05 05:28:45 +0900

    날씨완성

--- app/src/main/java/com/example/jaein/solarsee/PlaceInfo.java
+++ app/src/main/java/com/example/jaein/solarsee/PlaceInfo.java
@@ -11,18 +11,31 @@ public class PlaceInfo {
     private String p_placeName;
     public String p_sunRise;
     public String p_sunSet;
+    public String p_rtemp, p_stemp;
+    public String p_rcloud, p_scloud;
+    public int p_x;
+    public int p_y;
 
-    public PlaceInfo(LatLng latlng, String placeName){
+    public PlaceInfo(LatLng latlng, String placeName, int x, int y){
         this.p_latlng = latlng;
         this.p_placeName = placeName;
         p_sunRise = "";
         p_sunSet = "";
+        this.p_x = x;
+        this.p_y = y;
     }
 
 
-    public void setInfo(String sunrise, String sunset){
+    public void setSunInfo(String sunrise, String sunset){
         this.p_sunRise = sunrise;
         this.p_sunSet = sunset;
+    }
+
+    public void setWeatherInfo(String r_temper, String r_cloud, String s_temper, String s_cloud){
+        this.p_rtemp = r_temper;
+        this.p_rcloud = r_cloud;
+        this.p_stemp = s_temper;
+        this.p_scloud = s_cloud;
     }
 
     public LatLng getP_latlng(){
@@ -33,4 +46,10 @@ public class PlaceInfo {
     }
     public String getP_sunRise(){return p_sunRise;}
     public String getP_sunSet(){return p_sunSet;}
+    public String getP_rtemperature(){return p_rtemp;}
+    public String getP_rcloud(){return p_rcloud;}
+    public String getP_stemperature(){return p_stemp;}
+    public String getP_scloud(){return p_scloud;}
+    public int getP_x(){return  p_x;}
+    public int getP_y(){return p_y;}
 }
